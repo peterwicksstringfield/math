4e6219042447fb5b5262b98dfe72e70ef3ae2c45
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2020-03-17 11:24:21 -0500

    Copy is_mat_finite tests for is_scal_finite.

--- test/unit/math/prim/err/is_scal_finite_test.cpp
+++ test/unit/math/prim/err/is_scal_finite_test.cpp
@@ -22,3 +22,37 @@ TEST(ErrorHandlingScalar, isScalFinite_nan) {
   double nan = std::numeric_limits<double>::quiet_NaN();
   EXPECT_FALSE(is_scal_finite(nan));
 }
+
+TEST(ErrorHandlingScalar, isScalFinite_Matrix) {
+  Eigen::Matrix<double, Eigen::Dynamic, 1> x;
+
+  x.resize(3);
+  x << -1, 0, 1;
+  EXPECT_TRUE(is_scal_finite(x));
+
+  x.resize(3);
+  x << -1, 0, std::numeric_limits<double>::infinity();
+  EXPECT_FALSE(is_scal_finite(x));
+
+  x.resize(3);
+  x << -1, 0, -std::numeric_limits<double>::infinity();
+  EXPECT_FALSE(is_scal_finite(x));
+
+  x.resize(3);
+  x << -1, 0, std::numeric_limits<double>::quiet_NaN();
+  EXPECT_FALSE(is_scal_finite(x));
+}
+
+TEST(ErrorHandlingScalar, isScalFinite_Matrix_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  Eigen::Matrix<double, Eigen::Dynamic, 1> x_mat(3);
+  x_mat << nan, 0, 1;
+  EXPECT_FALSE(is_scal_finite(x_mat));
+
+  x_mat << 1, nan, 1;
+  EXPECT_FALSE(is_scal_finite(x_mat));
+
+  x_mat << 1, 0, nan;
+  EXPECT_FALSE(is_scal_finite(x_mat));
+}
